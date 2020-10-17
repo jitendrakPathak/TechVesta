3ad49186fb474c3f428be5f4d2772a03a0ae3d43
devashishofficial <devashishoffice@gmail.com>
2020-10-18 00:00:37 +0530

    Initial code

---
+++ TechVesta.Web/Controllers/PlanController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechVesta.Web.Controllers
+{
+    public class PlanController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Plans()
+        {
+            return View();
+        }
+
+        public IActionResult WebsitePlan()
+        {
+            return View();
+        }
+
+        public IActionResult HostingPlan()
+        {
+            return View();
+        }
+
+        public IActionResult SeoPlan()
+        {
+            return View();
+        }
+
+        public IActionResult SupportPlan()
+        {
+            return View();
+        }
+    }
+}
---
+++ TechVesta.Web/Controllers/ServiceController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechVesta.Web.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WebDevelopment()
+        {
+            return View();
+        }
+
+        public IActionResult SoftwareDevelopment()
+        {
+            return View();
+        }
+
+        public IActionResult WebSiteDesign()
+        {
+            return View();
+        }
+
+        public IActionResult Seo()
+        {
+            return View();
+        }
+    }
+}
